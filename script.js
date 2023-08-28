c36ca4308ca65ff8128ebf20db9eab624abccc94
Varun <Varun@Varuns-MacBook-Pro.local>
2023-08-28 22:19:32 +0530

    Added showing favorite meals functionality

--- script.js
+++ script.js
@@ -23,20 +23,18 @@ function showMealsOnSearch() {
                 }
                 if(isFavMeal)   classFav = "active";
                 html += 
-                    `<div id="card" class="card mb-3" style="width: 20rem;">
+                    `<div class="card mb-3" style="width: 20rem;">
                         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
                             <button type="button" class="btn btn-outline-dark" onclick="showMealDetails(${meal.idMeal})">More Details</button>
-                            <button id="main${meal.idMeal}" class="btn btn-outline-dark ${classFav}" onclick="addRemoveToFavList(${meal.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                            <button id="main${meal.idMeal}" class="btn btn-outline-dark ${classFav}" onclick="addRemoveToFavList(${meal.idMeal}, false)" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
                         </div>
                     </div>
                     `;
             });
         } else {
-            html += `
-                    <div> The meal you are looking for was not found. </div>
-                `;
+            html += `<div> The meal you are looking for was not found. </div>`;
         }
         document.getElementById("meal-cards").innerHTML = html;
     });
@@ -47,7 +45,7 @@ function showMealDetails(id) {
     let html = "";
     let meal = fetchMealsFromApi(fetchUrl, id);
     meal.then(data => {
-        console.log(data);
+        // console.log(data);
         html += `
                 <div id = "meal-header">
                 <div class="d-flex justify-content-around flex-wrap">
@@ -69,4 +67,49 @@ function showMealDetails(id) {
         document.getElementById('meal-cards').innerHTML = html;
     });
     
+}
+
+function showFavMeals() {
+    let favs = JSON.parse(localStorage.getItem("favMealsList"));
+    let html = "";
+    let fetchUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+    if(favs.length == 0) {
+        html += `<div> No meal added in your favourites list. </div>`;
+        document.getElementById('meal-cards').innerHTML = html;
+    } else {
+        for(var i = 0; i < favs.length; i++) {
+            let meal = fetchMealsFromApi(fetchUrl, favs[i]);
+            meal.then(data => {             
+                console.log(data);
+                    html += 
+                    `<div class="card mb-3" style="width: 20rem;">
+                        <img src="${data.meals[0].strMealThumb}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${data.meals[0].strMeal}</h5>
+                            <button type="button" class="btn btn-outline-dark" onclick="showMealDetails(${data.meals[0].idMeal})">More Details</button>
+                            <button id="main${data.meals[0].idMeal}" class="btn btn-outline-dark active" onclick="addRemoveToFavList(${data.meals[0].idMeal}, true)" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                    `;
+                    document.getElementById('meal-cards').innerHTML = html;
+            });
+        }
+    }
+}
+
+function addRemoveToFavList(id, isFav) {
+    let favs = JSON.parse(localStorage.getItem('favMealsList'));
+    let isPresent = false;
+    for(let i = 0; i < favs.length; i++) {
+        if(id == favs[i])   isPresent = true;
+    }
+    if(isPresent) {
+        let idx = favs.indexOf(id);
+        favs.splice(idx, 1);
+    } else {
+        favs.push(id);
+    }
+    localStorage.setItem('favMealsList', JSON.stringify(favs));
+    if(isFav) showFavMeals();
+    else showMealsOnSearch();
 }
